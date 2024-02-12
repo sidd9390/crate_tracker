4678cf6a507252591fec72ac003c677b9ab7e871
sidd93901~ <grandhisiddu99@gmail.com>
2024-02-12 18:03:12 +0530

    modified:   assets/css/style.css 	modified:   assets/js/script.js 	modified:   brand_owner.html 	new file:   distributorlist.html 	modified:   driver_pilot_sales.html 	modified:   home.html 	modified:   trace.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,30 @@ function activeLink() {
 list.forEach((item) =>
 item.addEventListener('click',activeLink));
 
-function f1(){
-    document.getElementById("shops").style.display ="none";
-    document.getElementById("distributors").style.display ="block";
-    document.getElementById("distributor_cat").style.backgroundColor = "rgb(19, 109, 128)"
-    document.getElementById("shop_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
-}
-function f2(){
-    document.getElementById("shops").style.display ="block";
-    document.getElementById("distributors").style.display ="none";
-    document.getElementById("shop_cat").style.backgroundColor = "rgb(19, 109, 128)"
-    document.getElementById("distributor_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
-}
 
 
 function f3(){
     document.getElementById("pilots").style.display ="block";
     document.getElementById("retail").style.display ="none";
+    document.getElementById("distri").style.display ="none";
     document.getElementById("sales_cat").style.backgroundColor = "rgb(19, 109, 128)"
     document.getElementById("retailer_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
+    document.getElementById("distribut_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
+
 }
 function f4(){
     document.getElementById("pilots").style.display ="none";
     document.getElementById("retail").style.display ="block";
+    document.getElementById("distri").style.display ="none";
     document.getElementById("retailer_cat").style.backgroundColor = "rgb(19, 109, 128)"
     document.getElementById("sales_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
+    document.getElementById("distribut_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
+}
+function f5(){
+    document.getElementById("pilots").style.display ="none";
+    document.getElementById("retail").style.display ="none";
+    document.getElementById("distri").style.display ="block";
+    document.getElementById("retailer_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
+    document.getElementById("sales_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
+    document.getElementById("distribut_cat").style.backgroundColor = "rgb(19, 109, 128)"
 }
