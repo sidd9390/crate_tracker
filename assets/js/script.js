bcaf82bf63a2a3e54338919d8412ebbf9a43a5a2
sidd93901~ <grandhisiddu99@gmail.com>
2024-02-12 11:54:27 +0530

    new file:   add_Distributor.html 	new file:   add_shop.html 	modified:   assets/css/style.css 	modified:   assets/js/script.js 	modified:   driver_pilot_sales.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,16 @@ function activeLink() {
 }
 list.forEach((item) =>
 item.addEventListener('click',activeLink));
+
+function f1(){
+    document.getElementById("shops").style.display ="none";
+    document.getElementById("distributors").style.display ="block";
+    document.getElementById("distributor_cat").style.backgroundColor = "rgb(19, 109, 128)"
+    document.getElementById("shop_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
+}
+function f2(){
+    document.getElementById("shops").style.display ="block";
+    document.getElementById("distributors").style.display ="none";
+    document.getElementById("shop_cat").style.backgroundColor = "rgb(19, 109, 128)"
+    document.getElementById("distributor_cat").style.backgroundColor = "rgba(27, 154, 179, 1)"
+}
